c3931947e5d1a581f9ac9409883904d5ed39083c
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2023-07-06 18:35:04 -0500

    made template for staff cards on team page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact= () => {
     return(
-        <div>
+        <div className="flex flex-col items-center  h-screen bg-cover bg-center bg-blue-950">
 
         </div>
     );
