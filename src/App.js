af30447a962fb6ba85ca1e447d6c9f937cc149a6
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2024-01-19 14:27:36 -0600

    final build push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { Form, Input, TextArea, Button } from "semantic-ui-react";
+// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Form, Input, TextArea, Button } from "semantic-ui-react";
 import Home from './components/Home';
 // import Navbar from './components/Navbar';
 // import Team from './components/Team';
